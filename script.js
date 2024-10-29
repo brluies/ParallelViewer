c021a3d76ac64af6017c0e7ad29644b101f100e3
brluies <123422292+brluies@users.noreply.github.com>
2024-10-29 15:12:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ async function loadTEI(file, elementId) {
     const bodyContent = xmlDoc.getElementsByTagName("body")[0];
     document.getElementById(elementId).innerHTML = bodyContent.innerHTML;
 }
-
-window.onload = () => {
-    loadTEI('tei_files/CaxtonGoldenLegend1483.xml', 'text1');
-    loadTEI('tei_files/SienaGoldenLegende.xml', 'text2');
+    window.onload = () => {
+    loadTEI('/parallel-viewer/tei_files/CaxtonGoldenLegend1483.xml', 'text1');
+    loadTEI('/parallel-viewer/tei_files/SienaGoldenLegende.xml', 'text2');
+};
 };
